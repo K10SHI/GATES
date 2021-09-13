13d770cf0cc0f717f01a93ced23e7e4230404473
K10SHI <89117005+K10SHI@users.noreply.github.com>
2021-09-13 18:40:41 +0800

    GATES

--- BUF GATE/BUF_GATE.v
+++ BUF GATE/BUF_GATE.v
@@ -3,8 +3,8 @@ always @ (A) begin
     if (A == 1'b1 ) begin                //states that if  A is 1
         Y = 1'b1;                        // then Y has to be 1
     end
-    else if (A == 1'b1) begin            //states that if  A is 1
-       Y = 1'b0;                         // then Y has to be 0
+    else if (A == 1'b0) begin            //states that if  A is 0
+       Y = 1'b1;                         // then Y has to be 0
 end
 end
 endmodule                                //used to terminate the module
--- BUF GATE/BUF_GATE_TEST_BENCH.v
+++ BUF GATE/BUF_GATE_TEST_BENCH.v
@@ -3,9 +3,9 @@ module BUF;
 reg A;wire Y;
 BUF_GATE Instance0 (Y, A);
 initial begin
-     A = 1;
-  #1 A = 0;
+     A = 0;
   #1 A = 1;
+  #1 A = 0;
  end
  initial begin
     $monitor ("%t | A = %d| Y = %d", $time, A, Y);
